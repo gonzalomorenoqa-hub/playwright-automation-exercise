33abab792f40b31f6ff629fde785bd3800d591fd
Gonzalo Moreno <gonzamoreno@MacBook-Pro-de-Gonzalo.local>
2026-05-01 18:23:02 -0300

    e2e tests branch initialized

--- tests/e2e/loginTests.spec.ts
+++ tests/e2e/loginTests.spec.ts
@@ -1,9 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
+//import { chromium } from 'playwright';
+import { PlaywrightBlocker } from '@cliqz/adblocker-playwright';
+import fetch from 'cross-fetch'; // Required for fetching filter lists
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('https://automationexercise.com/')
-})
+    //const browser = await chromium.launch({ headless: false });
+    //const context = await browser.newContext();
+    //const page = await context.newPage();
+
+    // Enable ad blocking
+    PlaywrightBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+        blocker.enableBlockingInPage(page);
+    });
+
+    await page.goto('https://automationexercise.com/');
+    // Ads and trackers will now be blocked
+});
+
+//await page.goto('https://automationexercise.com/')
+
 
 test('has title', async ({ page }) => {
 
-})
+});
