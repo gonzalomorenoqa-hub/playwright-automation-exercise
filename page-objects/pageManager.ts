3e05aea4e06ff071b3b56a1e48559fa435eb29b1
Gonzalo Moreno <gonzamoreno@MacBook-Pro-de-Gonzalo.local>
2026-05-04 12:27:57 -0300

    pageManager and homePage initialized

---
+++ page-objects/pageManager.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from "@playwright/test";
+import { HomePage } from "./homePage";
+
+export class PageManager{
+
+    private readonly page:Page
+    private readonly homePage: HomePage
+
+    constructor(page: Page){
+        this.page = page
+        this.homePage = new HomePage(this.page)
+    }
+
+    navigateTo(){
+        return this.homePage
+    }
+
+    async productsPage(){
+        return this.productsPage
+    }
+}
